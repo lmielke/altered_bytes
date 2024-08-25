b3c1d33faa4a0401d3a69f94830889b7a627c665
Lars Mielke <larsmielke2@gmail.com>
2024-08-25 14:00:02 +0200

    startup link uses env variable now log not found

--- altered/resources/startup/ollama_run.ps1
+++ altered/resources/startup/ollama_run.ps1
@@ -1,4 +1,6 @@
-# Change to the directory where your Pipenv environment and simple_server.py are located
+# ollama_run.ps1
+# You can cp the shortcut in this folder to the startup folder to run the server on startup.
+# See Readme.md
 cd "$env:altered_bytes"
 # Start the Python server script in detached mode using pipenv run
 Start-Process -FilePath "pipenv" -ArgumentList "run", "python", "./altered/simple_server.py" -WindowStyle Hidden
