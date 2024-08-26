63e784112d4339cf30fcf7ecdc48947cac4b1bae
Lars Mielke <larsmielke2@gmail.com>
2024-08-26 18:53:24 +0200

    ollama_run.ps1 changes winget unistall log not found

--- altered/resources/startup/ollama_run.ps1
+++ altered/resources/startup/ollama_run.ps1
@@ -44,10 +44,10 @@ Write-Output "ollama_run.ps1 script completed."
 Stop-Transcript
 
 # we also start docker in case openWebUi is used
-timeout /t 15
-winget uninstall Docker.DockerDesktop
-timeout /t 15
-Start-Process -FilePath "C:\Program Files\Docker\Docker\Docker Desktop.exe"
+# timeout /t 15
+# winget uninstall Docker.DockerDesktop
+# timeout /t 15
+# Start-Process -FilePath "C:\Program Files\Docker\Docker\Docker Desktop.exe"
 # timeout /t 120
 # docker run -d -p 3000:8080 --add-host=host.docker.internal:host-gateway -v open-webui:/app/backend/data --name open-webui --restart always ghcr.io/open-webui/open-webui:main
 
