1ba36fc8b52e28296dda7c23a3ca67bc7b24fd45
Lars Mielke <larsmielke2@gmail.com>
2024-09-13 18:18:47 +0200

    major restruct for server and client 5 [lut no stats]

--- altered/pre_ollama_server.py
+++ altered/pre_ollama_server.py
@@ -162,6 +162,7 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         # Update response with timing information and other server statistics
         payload.update(self.end_timing(*args, **kwargs))
         # Send the JSON response
+        print(f"do_POST.out: {payload = }")
         self.send_server_response(payload, *args, **kwargs)
 
     def get_kwargs(self, *args, **kwargs) -> dict:
