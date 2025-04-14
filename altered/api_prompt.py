785a7b048ea6265b8d16f5b2361a3ca57f791b71
Lars Mielke <larsmielke2@gmail.com>
2025-04-14 18:35:31 +0200

    Based on the provided context information, user comment, and instructions, here is a short git commit message:  `-m 'git diff'`[lut no stats]

--- altered/api_prompt.py
+++ altered/api_prompt.py
@@ -2,19 +2,22 @@
 api_prompt.py
 """
 
+from colorama import Fore, Style
 from datetime import datetime
 import json
+import sys
+
+# package imports
 import altered.settings as sts
 import altered.hlp_printing as hlpp
 from altered.prompt import Prompt
-from colorama import Fore, Style
 
-def prompt(*args, api:str, verbose:int, **kwargs):
+def prompt(*args, api:str, **kwargs):
     """
     Display basic information about the system and configuration
     Returns a dictionary containing the information
     """
-    return Prompt(api, *args, verbose=verbose, **kwargs)(*args, verbose=verbose, **kwargs)
+    return Prompt(api, *args, **kwargs)(*args, **kwargs)
     # hlpp.pretty_prompt(prompt.data, *args, verbose=verbose, **kwargs)
 
 def main(*args, **kwargs):
@@ -22,8 +25,9 @@ def main(*args, **kwargs):
     Main entry point for the prompt API
     Returns the result of the prompt function
     """
-    # return json.dumps(prompt(*args, **kwargs).data)
-    print("Test")
+    p = json.dumps(prompt(*args, **kwargs).data)
+    sys.stdout.write(f"{p}\n")
+    sys.stdout.flush()
 
 if __name__ == "__main__":
     main()
